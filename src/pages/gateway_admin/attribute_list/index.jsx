38557a4953bfc4a2978a6566ae776a4124a981fe
mor99 <37009721+mor99@users.noreply.github.com>
2020-05-29 17:56:02 +0800

    功能完善3.5

--- src/pages/gateway_admin/attribute_list/index.jsx
+++ src/pages/gateway_admin/attribute_list/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import React from 'react'
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
 import { PlusOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
